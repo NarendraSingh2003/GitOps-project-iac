7e31b0d347f436bbc88d3908c4b90719e1368495
narendrasin2003@gmail.com <narendrasin2003@gmail.com>
2025-04-03 16:26:38 +0530

    now correct

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,22 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.25.0"
     }
-
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5.1"
     }
-
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0.4"
     }
-
     cloudinit = {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.2"
     }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
@@ -31,10 +27,6 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-  required_version = ">= 1.6.3, <= 1.11.3"
+
+  required_version = ">=1.6.3, <=1.11.3"
 }
-
-
-##
-##
-##
